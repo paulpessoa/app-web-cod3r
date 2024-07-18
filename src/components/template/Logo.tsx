33dfba1d1da9db52e0644788b84cf2ed08a5b9ff
GEP\Paul.Pessoa <paulmspessoa@gmail.com>
2024-07-18 00:04:45 -0300

    logo component

---
+++ src/components/template/Logo.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { IconBrandAmazon } from "@tabler/icons-react";
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <div className="flex flex-col items-center mt-2">
+        <div className="text-xl leading-4">A Z</div>
+        <IconBrandAmazon size={40} stroke={1} className="-mt-2"/>
+      </div>
+    </Link>
+  );
+}
